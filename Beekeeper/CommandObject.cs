ec16b1f48c39d3640eba9688ade6ea8bfe9d634d
Meng Lin <mlin6436@gmail.com>
2013-06-05 14:00:47 +0100

    Add project

---
+++ Beekeeper/CommandObject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beekeeper
+{
+    public class CommandObject
+    {
+        public CommandOption Option { get; set; }
+    }
+
+    public enum CommandOption
+    {
+        CheckStatus = 1
+    }
+}
