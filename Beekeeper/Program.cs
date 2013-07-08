0b95e5c4979d5fd2e4c20dd6af04f3193a55a760
Meng Lin <mlin6436@gmail.com>
2013-07-08 14:44:10 +0100

    Refine file management function

--- Beekeeper/Program.cs
+++ Beekeeper/Program.cs
@@ -1,4 +1,7 @@
-﻿using Args.Help.Formatters;
+﻿using System.Configuration;
+using System.IO;
+using System.Linq;
+using Args.Help.Formatters;
 using Beekeeper.Common;
 using Beekeeper.Entities;
 using Beekeeper.Managers;
@@ -12,6 +15,10 @@ namespace Beekeeper
 {
     public class Program
     {
+        public static string FilePostfixPattern = ConfigurationManager.AppSettings["FilePostfixPattern"];
+        public static string SystemVolumeInformationFolder = ConfigurationManager.AppSettings["SystemVolumeInformationFolder"];
+        public static string RecycleBinFolder = ConfigurationManager.AppSettings["RecycleBinFolder"];
+
         public static void Main(string[] args)
         {
             log4net.Config.XmlConfigurator.Configure();
@@ -30,6 +37,10 @@ namespace Beekeeper
                     var formatter = new ConsoleHelpFormatter(80, 1, 5);
                     Console.WriteLine(formatter.GetHelp(help));
                 }
+                else if (command.Action.Equals(CommandOption.Test))
+                {
+                    Console.WriteLine("For testing purposes!!!");
+                }
                 else if (command.Action.Equals(CommandOption.CheckStatus))
                 {
                     if (String.IsNullOrEmpty(command.Directory))
@@ -38,8 +49,39 @@ namespace Beekeeper
                         return;
                     }
 
-                    var fileManager = new FileManager();
-                    fileManager.CheckStatus(command.Directory);
+                    Console.WriteLine("Searching '{0}' ...", command.Directory);
+                    var subDirectoryList = Directory.GetDirectories(command.Directory)
+                        .Select(d => new DirectoryInfo(d))
+                        .Where(d => !d.Name.Equals(SystemVolumeInformationFolder))
+                        .Where(d => !d.Name.Equals(RecycleBinFolder))
+                        .OrderBy(d => d.Name)
+                        .ToList();
+
+                    if (!subDirectoryList.Any())
+                    {
+                        Console.WriteLine("The directory is empty!");
+                        return;
+                    }
+
+                    Console.WriteLine("Directories found: {0}", subDirectoryList.Count);
+                    Console.WriteLine();
+                    foreach (var subDirectory in subDirectoryList)
+                    {
+                        Console.WriteLine("Searching '{0}' ...", subDirectory.Name);
+                        var files = Directory.GetFiles(subDirectory.FullName, FilePostfixPattern)
+                            .Select(f => new FileInfo(f))
+                            .OrderBy(f => f.LastWriteTime)
+                            .ToList();
+
+                        if (!files.Any())
+                        {
+                            Console.WriteLine("To process files found: {0}.", files.Count);
+                        }
+                        else
+                        {
+                            Console.WriteLine("To process files found: {0}, between '{1}' & '{2}'.", files.Count, files.FirstOrDefault().LastWriteTime.ToString("yyyymmdd HH:MM:ss"), files.LastOrDefault().LastWriteTime.ToString("yyyymmdd HH:MM:ss"));
+                        }
+                    }
                 }
                 else if (command.Action.Equals(CommandOption.DropDatabase))
                 {
